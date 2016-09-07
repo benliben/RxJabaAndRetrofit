680b8318e94dac4b7ae8285d566cbffb422272b1
junjie <1287550045@qq.com>
2016-09-07 17:26:56 +0800

    添加了单元测试的一个小案例

---
+++ app/src/main/java/cn/kiwano/benben/rxjabaandretrofit/Calculator.java
@@ -0,0 +1,25 @@
+package cn.kiwano.benben.rxjabaandretrofit;
+
+/**
+ * Created by LiYuanxiong on 2016/9/7 17:00.
+ * Desribe:
+ */
+public class Calculator {
+    public double sum(double a, double b){
+        return a+b;
+    }
+
+    public double substract(double a, double b){
+        return a-b;
+    }
+
+    public double divide(double a, double b){
+        return a/b;
+    }
+
+    public double multiply(double a, double b){
+        return a*b;
+    }
+
+
+}
